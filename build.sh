deb038ae30f100ed2c0163c69b33a0637819387d
Will Thames <will@thames.id.au>
2018-10-28 17:30:51 +1000

    Update to latest versions of kubernetes

--- build.sh
+++ build.sh
@@ -10,9 +10,16 @@
 REPO="garethr/kubernetes-json-schema"
 
 declare -a arr=(master
+                v1.12.2
+                v1.12.1
+                v1.12.0
+                v1.11.4
+                v1.11.3
                 v1.11.2
                 v1.11.1
                 v1.11.0
+                v1.10.9
+                v1.10.8
                 v1.10.7
                 v1.10.6
                 v1.10.5
@@ -21,6 +28,7 @@ declare -a arr=(master
                 v1.10.2
                 v1.10.1
                 v1.10.0
+                v1.9.11
                 v1.9.10
                 v1.9.9
                 v1.9.8
@@ -48,23 +56,6 @@ declare -a arr=(master
                 v1.8.2
                 v1.8.1
                 v1.8.0
-                v1.7.16
-                v1.7.15
-                v1.7.14
-                v1.7.13
-                v1.7.12
-                v1.7.11
-                v1.7.10
-                v1.7.9
-                v1.7.8
-                v1.7.7
-                v1.7.6
-                v1.7.5
-                v1.7.4
-                v1.7.3
-                v1.7.2
-                v1.7.1
-                v1.7.0
                 )
 
 for version in "${arr[@]}"
